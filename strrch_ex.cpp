12d6c04039b9e77060edd48379be412bdac39cef
krishmrajan <krishmrajan@users.noreply.github.com>
2019-05-14 22:50:00 +0200

    Create strrch_ex.cpp

---
+++ strrch_ex.cpp
@@ -0,0 +1,25 @@
+/* strrchr example */
+#include <stdio.h>
+#include <string.h>
+#include <libgen.h>
+
+int main ()
+{
+  char str[] = "/This/is/sample/string";
+  char * pch;
+  char str1[1024];
+  memset(str1,0, sizeof(str1));
+  pch=strrchr(str,'/');
+  while(*pch != '\0') {
+    printf("%c", *pch);
+    
+    // move the ptr pointer to the next memory location
+    pch++;
+  }
+  printf("\n");
+  strncpy ( str1, str, pch+2-str+2);
+  //printf("\ndirname %s \n", dirname(str));
+  printf("dirname %s\n",str1);
+  
+  return 0;
+}
